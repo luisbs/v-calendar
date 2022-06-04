746cf23b21391e561bae6a202cc4bf9cbd2808b8
luisbs <lmartinez@megacomercios.com>
2022-06-03 22:11:02 -0600

    refactor: change to es modules for tree-shacking

--- src/utils/_.ts
+++ src/utils/_.ts
@@ -1,31 +1,33 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 // Type utils
-export { default as isBoolean } from 'lodash/isBoolean';
-export { default as isNumber } from 'lodash/isNumber';
-export { default as isString } from 'lodash/isString';
-export { default as isArray } from 'lodash/isArrayLikeObject';
-export { default as isFunction } from 'lodash/isFunction';
-export { default as isUndefined } from 'lodash/isUndefined';
-import _isDate from 'lodash/isDate';
+export { default as isBoolean } from 'lodash-es/isBoolean';
+export { default as isNumber } from 'lodash-es/isNumber';
+export { default as isString } from 'lodash-es/isString';
+export { default as isArray } from 'lodash-es/isArrayLikeObject';
+export { default as isFunction } from 'lodash-es/isFunction';
+export { default as isUndefined } from 'lodash-es/isUndefined';
+import _isDate from 'lodash-es/isDate';
 
 // Number utils
-export { default as clamp } from 'lodash/clamp';
+export { default as clamp } from 'lodash-es/clamp';
 
 // Object utils
-export { default as get } from 'lodash/get';
-export { default as set } from 'lodash/set';
-export { default as mapValues } from 'lodash/mapValues';
-export { default as toPairs } from 'lodash/toPairs';
-export { default as defaults } from 'lodash/defaults';
-export { default as defaultsDeep } from 'lodash/defaultsDeep';
-export { default as pick } from 'lodash/pick';
-export { default as omit } from 'lodash/omit';
-import _has from 'lodash/has';
+export { default as get } from 'lodash-es/get';
+export { default as set } from 'lodash-es/set';
+export { default as mapValues } from 'lodash-es/mapValues';
+export { default as toPairs } from 'lodash-es/toPairs';
+export { default as defaults } from 'lodash-es/defaults';
+export { default as defaultsDeep } from 'lodash-es/defaultsDeep';
+export { default as pick } from 'lodash-es/pick';
+export { default as omit } from 'lodash-es/omit';
+import _has from 'lodash-es/has';
 
 // Collection utils
-export { default as map } from 'lodash/map';
-export { default as head } from 'lodash/head';
-export { default as last } from 'lodash/last';
-import _some from 'lodash/some';
+export { default as map } from 'lodash-es/map';
+export { default as head } from 'lodash-es/head';
+export { default as last } from 'lodash-es/last';
+import _some from 'lodash-es/some';
 
 // Type checkers
 export const getType = (value: any) =>
